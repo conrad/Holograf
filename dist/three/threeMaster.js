1dd1953fb9231e8c6cca9e17ff4ee95faa4cc990
mc <mc@mcs-MacBook-Pro.local>
2015-02-20 15:13:09 -0800

    merge edits
    Consolidates a lot of the Three files
    
    Consolidates three files, pulls out of dist folder, and concatenates in gulpfile
    
    adds to potential controls

--- dist/three/threeMaster.js
+++ dist/three/threeMaster.js
@@ -1,6 +1,7 @@
 // controls.js
 
 
+<<<<<<< HEAD
 // controls = new THREE.OrbitControls(camera, container);
 // controls.addEventListener( 'change', render );
 
@@ -19,6 +20,26 @@
 
 // // inside animate()
 // controls.update();
+=======
+controls = new THREE.OrbitControls(camera, container);
+controls.addEventListener( 'change', render );
+
+
+
+controls = new THREE.TrackballControls(camera);
+controls.rotateSpeed = 1.0;
+controls.zoomSpeed = 1.2;
+controls.panSpeed = 0.8;
+controls.noZoom = false;
+controls.noPan = false;
+controls.staticMoving = true;
+controls.dynamicDampingFactor = 0.3;
+
+
+
+// inside animate()
+controls.update();
+>>>>>>> merge edits
 
 // var renderer, scene, camera, controls;
 // var sceneRendered = false;
