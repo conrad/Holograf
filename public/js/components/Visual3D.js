95d18e09f96519139d355a7f16b403da73478f4f
Matt Conrad <mmconrad@gmail.com>
2015-02-25 11:58:39 -0800

    Sizes div for scene to fill screen before compile; controls disabled right now

--- public/js/components/Visual3D.js
+++ public/js/components/Visual3D.js
@@ -54,7 +54,7 @@ module.exports = React.createClass({
         <div id="three-scene" data={this.props.data} >
         <Button className="visualButton" bsStyle="primary" onClick={this.pauseScene} >Pause</Button>
         <Button className="visualButton" bsStyle="primary" onClick={this.expandScene} >Expand</Button>
-        </ div>
+        </div>
         <div id="three-modal"></div>
       </div>
     );
